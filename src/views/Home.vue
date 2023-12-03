c2486a25bfdee6d40df7b023389d79438408b01f
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-12-03 12:47:44 +0100

    Adjust Home view to carousel component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <HelloWorld />
+  <TheCarousel></TheCarousel>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import TheCarousel from '@/components/TheCarousel.vue';
 </script>
