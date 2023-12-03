087ebdd1a28d81b735a2a35c1e1920fe7d47d948
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-12-03 12:47:24 +0100

    Add navigation drawer

---
+++ src/layouts/default/NavigationDrawer.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-navigation-drawer expand-on-hover rail>
+        <!-- <v-list>
+            <v-list-item prepend-avatar="@/assets/profile.jpg" title="Gregory Carvalho"
+                subtitle="Computer Engineer"></v-list-item>
+        </v-list> -->
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-folder" title="Speakers" value="experience"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-multiple" title="Schedule" value="key-skills"></v-list-item>
+            <v-list-item prepend-icon="mdi-star" title="Subscribe" value="formation"></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped></style>
