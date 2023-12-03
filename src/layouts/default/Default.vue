8c22574c8bfe45ce1830af75383e0f5931de4dcb
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-12-03 12:47:09 +0100

    Adjuste default view layout

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,12 +1,15 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <v-app>
     <default-bar />
+    <!-- <default-drawer></default-drawer> -->
 
     <default-view />
   </v-app>
 </template>
 
 <script setup>
-  import DefaultBar from './AppBar.vue'
-  import DefaultView from './View.vue'
+import DefaultBar from './AppBar.vue'
+import DefaultView from './View.vue'
+// import DefaultDrawer from './NavigationDrawer.vue';
 </script>
