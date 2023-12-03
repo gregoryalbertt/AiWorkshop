483223f2abfb2db0b003d3bc71570621cb40fda0
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-12-03 12:48:47 +0100

    Adjust routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,41 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '',
-        name: 'Home',
+        path: "",
+        name: "Home",
         // route level code-splitting
         // this generates a separate chunk (Home-[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import('@/views/Home.vue'),
+        component: () => import("@/views/Home.vue"),
+      },
+      {
+        path: "speakers",
+        name: "Speakers",
+        component: () => import("@/views/TheSpeakers.vue"),
+      },
+      {
+        path: "schedule",
+        name: "Schedule",
+        component: () => import("@/views/TheSchedule.vue"),
+      },
+      {
+        path: "subscribe",
+        name: "Subscribe",
+        component: () => import("@/views/TheForm.vue"),
       },
     ],
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
