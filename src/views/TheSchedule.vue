dbdb61f387f642c05e6a27e0e5452e425f19bfad
Kamilla Tenório <enkiski@gmail.com>
2023-12-04 19:51:46 +0100

    Update in the workshop schedule

--- src/views/TheSchedule.vue
+++ src/views/TheSchedule.vue
@@ -1,92 +1,120 @@
 <template>
     <v-card color="rgb(0,0,0, 0.5)" class="h-100">
         <v-img gradient="to bottom, rgba(0,0,0,.2), rgba(255,255,255,.2)" class="background" src="/fu-university.jpeg">
-
-            <div class="text-h2 font-weight-black text-center ma-4 pa-4">
-                Schedule
-            </div>
             <v-row>
 
                 <v-col cols="12" sm="6">
                     <v-card class="ma-10 pa-8">
                         <div class="text-h3 font-weight-black text-center">
-                            Workshop - Part 1
-                        </div> <br>
-
+                            Session I
+                        </div> 
 
                         <div class="text-subtitle-1 font-weight-black text-center">
                             Königin-Luise-Straße 24/26, Freie Universität Berlin
                         </div><br>
-                        <div class="text-h6 font-weight-black">
-                            09:00 - 10:00
+                        
+                        <div class="text-h6">
+                            09:30 - 10:00
                         </div>
                         <div>
                             Welcome Coffee
                         </div><br>
-                        <div class="text-h6 font-weight-black">
+                        <div class="text-h6">
                             10:00 - 10:10
                         </div>
                         <div>
                             Opening and greetings from the organizers
                         </div><br>
-                        <div class="text-h6 font-weight-black">
+                        <div class="text-h6">
                             10:10 - 10:30
                         </div>
-                        <div>
-                            Learning about AI - why it is important and what student can learn?
-                        </div><br>
-                        <div class="text-h6 font-weight-black">
-                            10:10 - 10:30
+                        <div class="font-weight-black">
+                            Learning about AI - why it is important and what student can learn? (German) <br>
                         </div>
                         <div>
-                            Learning about AI - why it is important and what student can learn? Prof. Dr. Ralf Romeike -
-                            Computing
-                            Education Research Group - Freie Universität Berlin
+                            Prof. Dr. Ralf Romeike - Computing Education Research Group - Freie Universität Berlin
                         </div><br>
-                        <div class="text-h6 font-weight-black">
+                        <div class="text-h6">
                             10:30 - 10:50
                         </div>
-                        <div>
-                            Experiences from AI in teacher education - AI Unplugged.
+                        <div class="font-weight-black">
+                            Experiences from AI in teacher education - AI Unplugged. (German) <br>
+                        </div>
+                        <div> 
                             Prof. Dr. Tilman Michaeli - Computing Education Research Group - Technische Universität München
                         </div><br>
-                        <div class="text-h6 font-weight-black">
+                        <div class="text-h6">
                             10:50 - 11:10
                         </div>
-                        <div>
-                            AI is changing the disciplines: Why and what teach for Non-CS students at university?
-                            Kamilla Tenório - Researcher - Computing Education Research Group - Freie Universität Berlin
-                        </div><br>
-                        <div class="text-h6 font-weight-black">
-                            11:10 - 11:30
+                        <div class="font-weight-black">
+                            AI is changing the disciplines: Why and what should university students learn about AI? (English) <br>
                         </div>
                         <div>
-                            Approaches and goals for implementing AI foundational at Freie Universität Berlin.
+                            Kamilla Tenório - Researcher - Computing Education Research Group - Freie Universität Berlin
+                        </div><br>
+                        <div class="text-h6">
+                            11:10 - 11:30
+                        </div>
+                        <div class="font-weight-black">
+                            Approaches and goals for implementing AI foundational at Freie Universität Berlin. (German) <br>
+                        </div>
+                        <div>
                             Uwe Lorenz - Researcher - Computing Education Research Group - Freie Universität Berlin
                         </div><br>
-
+                        <div class="text-h6">
+                            11:30 - 11:50
+                        </div>
+                        <div class="font-weight-black">
+                            Responsible AI (German) <br>
+                        </div>
+                        <div>
+                            Prof. Dr. Claudia Müller-Birn -  Human-Centered Computing (HCC) - Freie Universität Berlin
+                        </div><br>
+                        <div class="text-h6">
+                            11:50 - 12:00
+                        </div>
+                        <div>
+                            Final Discussion
+                        </div><br>
                     </v-card>
                 </v-col>
                 <v-col cols="12" sm="">
                     <v-card class="ma-10 pa-8">
-                        <span class="text-h3">
-                            Workshop - Part 2
-                        </span>
-                        Fabeckstraße 35 - Raum 001, Freie Universität Berlin
+                        <div class="text-h3 font-weight-black text-center">
+                            Session II
+                        </div> 
 
-                        14:15 - 15:15
-                        Keynote
-                        "Learning Designs in the Time of Artificial Intelligence and Machine Learning"
-                        Yasmin B. Kafai - Distinguished Professor in the Graduate School of Education at the University of
-                        Pennsylvania
+                        <div class="text-subtitle-1 font-weight-black text-center">
+                            Fabeckstraße 35 - Raum 001, Freie Universität Berlin
+                        </div><br>
 
-                        15:15 -16:00
-                        Discussion and questions.
+                        <div class="text-h6">
+                            14:15 - 15:15
+                        </div>
+                        <div class="font-weight-black">
+                            Keynote <br>
+                            "Learning Designs in the Time of Artificial Intelligence and Machine Learning" <br>
+                        </div>
+                        <div>
+                            Yasmin B. Kafai - Distinguished Professor in the Graduate School of Education at the University of Pennsylvania
+                        </div><br>
+                        
+                        <div class="text-h6">
+                            15:15 -16:00
+                        </div>
+                        <div>
+                            Discussion and questions.
+                        </div><br>
 
-                        16:00 - 18:00
-                        Networking Time
-                        Poster Presentations (AI Education Research at Freie Universität Berlin)
-                        Closure Coffee Time
+                        <div class="text-h6">
+                            16:00 - 18:00
+                        </div>
+                        <div>
+                            Networking Time <br>
+                            Poster Presentations (AI Education Research at Freie Universität Berlin) <br>
+                            Closure Coffee Time
+                        </div><br>
+
                     </v-card>
                 </v-col>
             </v-row>
