0f385a32f217bc7204d57a64f1e615ed1b543e8b
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-12-04 19:20:28 +0100

    Adjust Speaker section

---
+++ src/components/speakers/BaseSpeaker.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/require-prop-type-constructor -->
+<template>
+    <v-card color="grey-lighten-1" :class="['ma-4', selectedClass]" height="300" width="240" @click="toggle">
+        <div class="d-flex fill-height align-center justify-center">
+            <!-- <v-scale-transition>
+                <v-icon v-if="isSelected" color="white" size="48" icon="mdi-close-circle-outline"></v-icon>
+            </v-scale-transition> -->
+        </div>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        isSelected: { type: Boolean },
+        toggle: { type: Function },
+        // eslint-disable-next-line vue/require-prop-type-constructor
+        selectedClass: "",
+    }
+
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/TheSpeakers.vue
+++ src/views/TheSpeakers.vue
@@ -1,15 +1,26 @@
 <template>
-    <v-sheet ref="speakers" class="mx-auto" elevation="8" max-width="1600">
+    <v-sheet ref="speakers" class="mx-auto" elevation="8" max-width="1600" pa-2 ma-2>
         <div class="text-h2 font-weight-black text-center ma-4 pa-4">
             Speakers
         </div>
+        <div class="text-4 ma-2 pa-2">
+            Lorem ipsum
+
+        </div>
         <v-slide-group v-model="model" class="pa-4" selected-class="bg-primary" show-arrows>
-            <v-slide-group-item v-for="n in 5" :key="n" v-slot="{ isSelected, toggle, selectedClass }">
-                <v-card color="grey-lighten-1" :class="['ma-4', selectedClass]" height="300" width="240" @click="toggle">
+            <v-slide-group-item v-for="(speaker) in speakers" :key="speaker.name"
+                v-slot="{ isSelected, toggle, selectedClass, }">
+                <v-card :image="speaker.picture" :class="['ma-4', selectedClass]" height="300" width="240" @click="toggle">
                     <div class="d-flex fill-height align-center justify-center">
                         <v-scale-transition>
-                            <v-icon v-if="isSelected" color="white" size="48" icon="mdi-close-circle-outline"></v-icon>
+                            <!-- <v-icon v-if="isSelected" color="white" size="48" icon="mdi-close-circle-outline"></v-icon>
+                             -->
+
                         </v-scale-transition>
+                        <v-overlay :model-value="isSelected" contained scrim="#036358" class="align-center justify-center">
+                            <!-- <v-btn variant="flat">See more info</v-btn> -->
+                        </v-overlay>
+
                     </div>
                 </v-card>
             </v-slide-group-item>
@@ -19,7 +30,19 @@
             <v-sheet v-if="model != null" height="200">
                 <div class="d-flex fill-height align-center justify-center">
                     <h3 class="text-h6">
-                        Selected {{ model }}
+                        <div class="text-h4">
+                            {{ speakers[model].name }}
+                        </div>
+                        <div class="text-h5">
+                            {{ speakers[model].role }}
+                        </div>
+                        <div class="text-h5">
+                            {{ speakers[model].department }}
+                        </div>
+                        <div class="text-h5">
+                            {{ speakers[model].university }}
+                        </div>
+
                     </h3>
                 </div>
             </v-sheet>
@@ -31,7 +54,59 @@
 export default {
     data: () => ({
         model: null,
+        speakers: [
+            {
+                name: "Prof. Dr. Ralf Romeike",
+                role: "Professor",
+                department: "Computing Education Research Group",
+                university: "Freie Universität Berlin",
+                description: "description",
+                picture: "ralf.png"
+            },
+            {
+                name: "Kamilla Tenório",
+                role: "Researcher",
+                department: "Computing Education Research Group",
+                university: "Freie Universität Berlin",
+                description: "description",
+                picture: "kamis.png"
+            },
+            {
+                name: "Uwe Lorenz",
+                role: "Researcher",
+                department: "Computing Education Research Group",
+                university: "Freie Universität Berlin",
+                description: "description",
+                picture: "uwe.png"
+            },
+            {
+                name: "Kamis",
+                role: "role",
+                department: "Computing Education Research Group",
+                description: "description",
+                picture: "kamis"
+            },
+            {
+                name: "Kamis",
+                role: "role",
+                department: "Computing Education Research Group",
+                description: "description",
+                picture: "kamis"
+            },
+        ],
     }),
+    computed: {
+        speakerImage() {
+            if (this.model) {
+                console.log(this.model);
+                return this.speakers[this.model].name
+            }
+            else {
+                return 'kamis'
+            }
+
+        }
+    }
 }
 </script>
 
