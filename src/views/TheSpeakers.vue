7bb33145f5d944f1846fb5d22e15eca0e0414899
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-12-04 23:45:39 +0100

    Adjust Speakers style

--- src/views/TheSpeakers.vue
+++ src/views/TheSpeakers.vue
@@ -1,53 +1,58 @@
 <template>
-    <v-sheet ref="speakers" class="mx-auto" elevation="8" max-width="1600" pa-2 ma-2>
-        <div class="text-h2 font-weight-black text-center ma-4 pa-4">
-            Speakers
-        </div>
-        <div class="text-4 ma-2 pa-2">
-            Lorem ipsum
+    <v-card color="rgb(0,0,0, 0.5)" class="h-100">
+        <v-img gradient="to bottom, rgba(0,0,0,.2), rgba(255,255,255,.2)" class="background" src="/fu-university.jpeg">
 
-        </div>
-        <v-slide-group v-model="model" class="pa-4" selected-class="bg-primary" show-arrows>
-            <v-slide-group-item v-for="(speaker) in speakers" :key="speaker.name"
-                v-slot="{ isSelected, toggle, selectedClass, }">
-                <v-card :image="speaker.picture" :class="['ma-4', selectedClass]" height="300" width="240" @click="toggle">
-                    <div class="d-flex fill-height align-center justify-center">
-                        <v-scale-transition>
-                            <!-- <v-icon v-if="isSelected" color="white" size="48" icon="mdi-close-circle-outline"></v-icon>
+
+            <v-sheet ref="speakers" class="mx-auto mt-10" elevation="8" max-width="1600" pa-2 ma-2>
+                <div class="text-h2 font-weight-black text-center mt-4 pt-4">
+                    Speakers
+                </div>
+                <v-slide-group v-model="model" class="pa-4" selected-class="bg-primary" show-arrows>
+                    <v-slide-group-item v-for="(speaker) in speakers" :key="speaker.name"
+                        v-slot="{ isSelected, toggle, selectedClass, }">
+                        <v-card :image="speaker.picture" :class="['ma-4', selectedClass]" height="300" width="240"
+                            @click="toggle">
+                            <div class="d-flex fill-height align-center justify-center">
+                                <v-scale-transition>
+                                    <!-- <v-icon v-if="isSelected" color="white" size="48" icon="mdi-close-circle-outline"></v-icon>
                              -->
 
-                        </v-scale-transition>
-                        <v-overlay :model-value="isSelected" contained scrim="#036358" class="align-center justify-center">
-                            <!-- <v-btn variant="flat">See more info</v-btn> -->
-                        </v-overlay>
+                                </v-scale-transition>
+                                <v-overlay :model-value="isSelected" contained scrim="#036358"
+                                    class="align-center justify-center">
+                                    <!-- <v-btn variant="flat">See more info</v-btn> -->
+                                </v-overlay>
 
-                    </div>
-                </v-card>
-            </v-slide-group-item>
-        </v-slide-group>
+                            </div>
+                        </v-card>
+                    </v-slide-group-item>
+                </v-slide-group>
 
-        <v-expand-transition>
-            <v-sheet v-if="model != null" height="200">
-                <div class="d-flex fill-height align-center justify-center">
-                    <h3 class="text-h6">
-                        <div class="text-h4">
-                            {{ speakers[model].name }}
-                        </div>
-                        <div class="text-h5">
-                            {{ speakers[model].role }}
-                        </div>
-                        <div class="text-h5">
-                            {{ speakers[model].department }}
-                        </div>
-                        <div class="text-h5">
-                            {{ speakers[model].university }}
-                        </div>
+                <v-expand-transition>
+                    <v-sheet v-if="model != null" height="200">
+                        <div class="d-flex fill-height align-center justify-center">
+                            <h3 class="text-h6">
+                                <div class="text-h4 font-weight-black">
+                                    {{ speakers[model].name }}
+                                </div>
+                                <div class="text-h5">
+                                    {{ speakers[model].role }}
+                                </div>
+                                <div class="text-h5">
+                                    {{ speakers[model].department }}
+                                </div>
+                                <div class="text-h5">
+                                    {{ speakers[model].university }}
+                                </div>
 
-                    </h3>
-                </div>
+                            </h3>
+                        </div>
+                    </v-sheet>
+                </v-expand-transition>
             </v-sheet>
-        </v-expand-transition>
-    </v-sheet>
+
+        </v-img>
+    </v-card>
 </template>
 
 <script>
