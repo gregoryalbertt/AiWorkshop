92431a403d23dc2c081910a882fd33abbfda3b20
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-12-04 22:10:00 +0100

    Adjust home Page

--- src/components/TheCarousel.vue
+++ src/components/TheCarousel.vue
@@ -22,12 +22,98 @@
     <v-card cover height="100%" image="/fu-university.jpeg">
 
 
-        <v-card>
 
 
-        </v-card>
+        <!-- <v-sheet :rounded=rounded :elevation="1" :height="200" :width="300" color="rgb(255,255,255,0.1)"></v-sheet> -->
 
-        <div class="container">
+        <base-card class="text-format">
+
+            <section>
+
+                <v-row>
+                    <div class="text-h2 text-center mx-auto font-weight-black">
+                        AI EDUCATION DAY
+                    </div>
+                </v-row>
+
+                <v-row>
+                    <span class="text-h4 text-center mx-auto">
+                        AT
+                    </span>
+                    <span class="text-h4 text-center mx-auto">
+                        FREIE UNIVERSITÄT BERLIN
+                    </span>
+                </v-row>
+                <v-row>
+                    <div class="text-h5 text-center mx-auto">
+                        with Professor Yasmin B. Kafai (University of Pennsylvania) as keynote speake
+                    </div>
+                </v-row>
+            </section>
+
+            <section>
+                <v-row>
+                    <div class="text-h4 text-center mx-auto font-weight-black">
+                        JANUARY 18TH, 2024
+                    </div>
+                </v-row>
+
+                <v-row>
+                    <div class="text-h5 text-center mx-auto">
+                        09:30 a.m to 05:30pm
+                    </div>
+                </v-row>
+            </section>
+
+
+            <section>
+                <v-row>
+                    <div class="text-h4 text-center mx-auto font-weight-black">
+                        LOCAL
+                    </div>
+                </v-row>
+
+                <v-row>
+                    <div class="text-h5 text-center mx-auto">
+                        Session I: Königin-Luise-Str. 24/26
+                    </div>
+                </v-row>
+                <v-row>
+                    <div class="text-h5 text-center mx-auto">
+                        Session II: Fabeckstraße 35 - Room 001
+                    </div>
+                </v-row>
+            </section>
+
+            <section>
+                <v-row>
+                    <v-col>
+                        <div class="text-h5">
+                            FUNDED BY
+                        </div>
+                    </v-col>
+                    <v-col>
+                        <v-img src="/BMBF-Logo.png">
+                        </v-img>
+                    </v-col>
+                    <v-col>
+                        <v-img src="/EU-logo.png">
+                        </v-img>
+                    </v-col>
+                    <v-col>
+                        <v-img src="/Berlin-Ressortlogo.png">
+                        </v-img>
+                    </v-col>
+                </v-row>
+            </section>
+
+
+
+
+
+
+        </base-card>
+        <!-- <div class="container">
             <div class="row">
                 <div class="text-h2 text-center mx-auto font-weight-black">
                     Collaborative Workshop
@@ -50,15 +136,22 @@
                     14195 Berlin
                 </div>
             </div>
+        </div> -->
 
 
-        </div>
 
     </v-card>
 </template>
 
 <script>
+import BaseCard from '@/layouts/default/BaseCard.vue'
 export default {
+    data() {
+        return {
+            rounded: 'xl',
+        }
+    },
+    components: { BaseCard }
 
 }
 </script>
@@ -69,29 +162,13 @@ export default {
     margin: 20%;
 }
 
-.text-h1 {
-    text-shadow: 2px 2px 4px #000000;
+.text-format {
+    color: rgb(0, 0, 0);
+    background-color: rgba(245, 245, 245, 0.75);
+    margin-top: 10%;
 }
 
-.text-h2 {
-    text-shadow: 2px 2px 4px #000000;
-    padding: 1rem;
-    background-color: rgba(245, 245, 245, 0.23);
-}
-
-.text-h3 {
-    text-shadow: 2px 2px 4px #000000;
-    padding: 1rem;
-    background-color: rgba(245, 245, 245, 0.33);
-}
-
-.text-h4 {
-    text-shadow: 2px 2px 4px #000000;
-    background-color: rgba(245, 245, 245, 0.33);
-}
-
-.text-h5 {
-    text-shadow: 2px 2px 4px #000000;
-    background-color: rgba(245, 245, 245, 0.43);
+section {
+    margin: 4rem;
 }
 </style>
