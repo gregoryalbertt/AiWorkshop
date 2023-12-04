aa056d1eb7f57adcfa5daaae613be755110ff590
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-12-04 19:21:05 +0100

    Ajduste Schedule page

--- src/views/TheSchedule.vue
+++ src/views/TheSchedule.vue
@@ -1,91 +1,97 @@
 <template>
-    <div class="text-h2 font-weight-black text-center ma-4 pa-4">
-        Schedule
-    </div>
-    <v-row>
+    <v-card color="rgb(0,0,0, 0.5)" class="h-100">
+        <v-img gradient="to bottom, rgba(0,0,0,.2), rgba(255,255,255,.2)" class="background" src="/fu-university.jpeg">
 
-        <v-col cols="12" sm="6">
-            <v-card class="ma-4 pa-4 text-center">
-                <span class="text-h3 font-weight-black">
-                    Workshop - Part 1
-                </span> <br>
+            <div class="text-h2 font-weight-black text-center ma-4 pa-4">
+                Schedule
+            </div>
+            <v-row>
+
+                <v-col cols="12" sm="6">
+                    <v-card class="ma-10 pa-8">
+                        <div class="text-h3 font-weight-black text-center">
+                            Workshop - Part 1
+                        </div> <br>
 
 
-                <div class="text-subtitle-1 font-weight-black">
-                    Königin-Luise-Straße 24/26, Freie Universität Berlin
-                </div>
-                <span class="text-subtitle-1 font-weight-black">
-                    09:00 - 10:00
-                </span>
-                <span>
-                    - Welcome Coffee
-                </span><br>
-                <span class="text-subtitle-1 font-weight-black">
-                    10:00 - 10:10
-                </span>
-                <span>
-                    - Opening and greetings from the organizers
-                </span><br>
-                <span class="text-subtitle-1 font-weight-black">
-                    10:10 - 10:30
-                </span>
-                <span>
-                    Learning about AI - why it is important and what student can learn?
-                </span><br>
-                <span class="text-subtitle-1 font-weight-black">
-                    10:10 - 10:30
-                </span>
-                <span>
-                    Learning about AI - why it is important and what student can learn? Prof. Dr. Ralf Romeike - Computing
-                    Education Research Group - Freie Universität Berlin
-                </span><br>
-                <span class="text-subtitle-1 font-weight-black">
-                    10:30 - 10:50
-                </span>
-                <span>
-                    Experiences from AI in teacher education - AI Unplugged.
-                    Prof. Dr. Tilman Michaeli - Computing Education Research Group - Technische Universität München
-                </span><br>
-                <span class="text-subtitle-1 font-weight-black">
-                    10:50 - 11:10
-                </span>
-                <span>
-                    AI is changing the disciplines: Why and what teach for Non-CS students at university?
-                    Kamilla Tenório - Researcher - Computing Education Research Group - Freie Universität Berlin
-                </span><br>
-                <span class="text-subtitle-1 font-weight-black">
-                    11:10 - 11:30
-                </span>
-                <span>
-                    Approaches and goals for implementing AI foundational at Freie Universität Berlin.
-                    Uwe Lorenz - Researcher - Computing Education Research Group - Freie Universität Berlin
-                </span><br>
+                        <div class="text-subtitle-1 font-weight-black text-center">
+                            Königin-Luise-Straße 24/26, Freie Universität Berlin
+                        </div><br>
+                        <div class="text-h6 font-weight-black">
+                            09:00 - 10:00
+                        </div>
+                        <div>
+                            Welcome Coffee
+                        </div><br>
+                        <div class="text-h6 font-weight-black">
+                            10:00 - 10:10
+                        </div>
+                        <div>
+                            Opening and greetings from the organizers
+                        </div><br>
+                        <div class="text-h6 font-weight-black">
+                            10:10 - 10:30
+                        </div>
+                        <div>
+                            Learning about AI - why it is important and what student can learn?
+                        </div><br>
+                        <div class="text-h6 font-weight-black">
+                            10:10 - 10:30
+                        </div>
+                        <div>
+                            Learning about AI - why it is important and what student can learn? Prof. Dr. Ralf Romeike -
+                            Computing
+                            Education Research Group - Freie Universität Berlin
+                        </div><br>
+                        <div class="text-h6 font-weight-black">
+                            10:30 - 10:50
+                        </div>
+                        <div>
+                            Experiences from AI in teacher education - AI Unplugged.
+                            Prof. Dr. Tilman Michaeli - Computing Education Research Group - Technische Universität München
+                        </div><br>
+                        <div class="text-h6 font-weight-black">
+                            10:50 - 11:10
+                        </div>
+                        <div>
+                            AI is changing the disciplines: Why and what teach for Non-CS students at university?
+                            Kamilla Tenório - Researcher - Computing Education Research Group - Freie Universität Berlin
+                        </div><br>
+                        <div class="text-h6 font-weight-black">
+                            11:10 - 11:30
+                        </div>
+                        <div>
+                            Approaches and goals for implementing AI foundational at Freie Universität Berlin.
+                            Uwe Lorenz - Researcher - Computing Education Research Group - Freie Universität Berlin
+                        </div><br>
 
-            </v-card>
-        </v-col>
-        <v-col cols="12" sm="">
-            <v-card class="ma-4 pa-4">
-                <span class="text-h3">
-                    Workshop - Part 2
-                </span>
-                Fabeckstraße 35 - Raum 001, Freie Universität Berlin
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="">
+                    <v-card class="ma-10 pa-8">
+                        <span class="text-h3">
+                            Workshop - Part 2
+                        </span>
+                        Fabeckstraße 35 - Raum 001, Freie Universität Berlin
 
-                14:15 - 15:15
-                Keynote
-                "Learning Designs in the Time of Artificial Intelligence and Machine Learning"
-                Yasmin B. Kafai - Distinguished Professor in the Graduate School of Education at the University of
-                Pennsylvania
+                        14:15 - 15:15
+                        Keynote
+                        "Learning Designs in the Time of Artificial Intelligence and Machine Learning"
+                        Yasmin B. Kafai - Distinguished Professor in the Graduate School of Education at the University of
+                        Pennsylvania
 
-                15:15 -16:00
-                Discussion and questions.
+                        15:15 -16:00
+                        Discussion and questions.
 
-                16:00 - 18:00
-                Networking Time
-                Poster Presentations (AI Education Research at Freie Universität Berlin)
-                Closure Coffee Time
-            </v-card>
-        </v-col>
-    </v-row>
+                        16:00 - 18:00
+                        Networking Time
+                        Poster Presentations (AI Education Research at Freie Universität Berlin)
+                        Closure Coffee Time
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-img>
+    </v-card>
 </template>
 
 <script>
@@ -93,3 +99,14 @@ export default {
 
 }
 </script>
+
+<style scoped>
+.background {
+    background-color: white;
+}
+
+span {
+    padding: 1rem;
+    margin: 1rem;
+}
+</style>
