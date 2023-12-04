8e2168fcfa0249b3aa48e61049a4cc9d2b90d525
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-12-04 19:20:45 +0100

    Adjust navigation bar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,46 +1,129 @@
 <template>
-  <v-app-bar class="bring-back" absolute flat color="rgba(255, 255, 255, 0.1)">
-    <v-app-bar-title>
-
-      <!-- <v-icon icon="mdi-circle-slice-6" /> -->
+  <v-app-bar image="/app-bar.jpeg" flat color="rgba(255, 255, 255, 0.01)">
 
 
-      <div class="row mx-auto d-flex justify-center ma-2 pa-2">
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn color="primary" v-bind="props">
+          <template v-slot:prepend>
+            <v-app-bar-nav-icon>
+            </v-app-bar-nav-icon>
+          </template>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>
+            <router-link :to="{ name: 'Home' }">
+              Home
+            </router-link>
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>
+            <router-link :to="{ name: 'Schedule' }">
+              Schedule
+            </router-link>
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>
+            <router-link :to="{ name: 'Speakers' }">
+              Speakers
+            </router-link>
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>
+            <router-link :to="{ name: 'Subscribe' }">
+              Register
+            </router-link>
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
 
-        <v-btn class="ma-3 pa-3" rounded="lg" variant="outlined">
-          <router-link :to="{ name: 'Home' }">Home</router-link>
-        </v-btn>
-        <v-btn class="ma-3 pa-3" rounded="lg" variant="outlined">
-          <router-link :to="{ name: 'Schedule' }">Schedule</router-link>
-        </v-btn>
-        <v-btn class="ma-3 pa-3" rounded="lg" variant="outlined">
-          <router-link :to="{ name: 'Speakers' }">Speakers</router-link>
-        </v-btn>
-        <v-btn class="ma-3 pa-3" rounded="lg" variant="outlined">
-          <router-link :to="{ name: 'Subscribe' }">Register</router-link>
-        </v-btn>
+    <v-container class="d-none d-lg-block">
+      <v-row no-gutters>
+        <v-col cols="12" sm="1">
+
+          <v-app-bar-title>
+            <!-- <v-btn icon="$vuetify">
+              Button
+            </v-btn> -->
+          </v-app-bar-title>
+        </v-col>
+        <v-col cols="12" sm="11">
+          <div class="row mx-auto d-flex justify-center">
+
+            <router-link class="text-decoration-none" :to="{ name: 'Home' }">
+              <h4>HOME</h4>
+            </router-link>
+            <router-link class="text-decoration-none" :to="{ name: 'Schedule' }">
+              <h4>SCHEDULE</h4>
+            </router-link>
+            <router-link class="text-decoration-none" :to="{ name: 'Speakers' }">
+              <h4>SPEAKERS</h4>
+            </router-link>
+            <router-link class="text-decoration-none" :to="{ name: 'Subscribe' }">
+              <h4>REGISTER</h4>
+            </router-link>
+
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+
+
+
+
+
+
+    <div class="row">
+      <div class="col">
+
       </div>
 
+      <div class="col">
+
+      </div>
+    </div>
 
 
-    </v-app-bar-title>
+
+
+
   </v-app-bar>
 </template>
 
 <script>
-
 export default {
-  methods: {
-    scrollToSubscribe() {
-      this.$refs.subscribe.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
+  data: () => ({
+    items: [
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me 2' },
+    ],
+  }),
 }
-
 </script>
 
 <style scoped>
 bring-back {
   z-index: -1;
+}
+
+.router-link-exact-active h4 {
+  /* background-color: rgb(188, 188, 188); */
+  text-decoration: underline;
+  font-weight: bold;
+}
+
+h4 {
+  padding: 1rem;
+  color: black;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
 }
 </style>
