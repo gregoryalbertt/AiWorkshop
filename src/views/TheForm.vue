497685c3b84efba28c8baca550ad34d6177124fc
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-12-05 03:23:18 +0100

    Install swal and implement in the form page

--- src/views/TheForm.vue
+++ src/views/TheForm.vue
@@ -1,30 +1,30 @@
 <template>
     <v-main class="background-image">
         <v-container>
-                <v-card class="text-format  rounded-lg rounded-md-xl pa-8 ma-4 pa-md-10 mt-10 mx-lg-auto">
-                    <div ref="subscribe" class="text-h4 text-lg-h2 font-weight-black text-center mb-4 pb-4">
-                        Registration
-                    </div>
-                    <form @submit.prevent="submit">
-                        <v-text-field v-model="name.value.value" :counter="10" :error-messages="name.errorMessage.value"
-                            label="Name"></v-text-field>
+            <v-card class="text-format  rounded-lg rounded-md-xl pa-8 ma-4 pa-md-10 mt-10 mx-lg-auto">
+                <div ref="subscribe" class="text-h4 text-lg-h2 font-weight-black text-center mb-4 pb-4">
+                    Registration
+                </div>
+                <form @submit.prevent="submit">
+                    <v-text-field v-model="name.value.value" :counter="10" :error-messages="name.errorMessage.value"
+                        label="Name"></v-text-field>
 
-                        <v-text-field v-model="email.value.value" :error-messages="email.errorMessage.value"
-                            label="E-mail"></v-text-field>
+                    <v-text-field v-model="email.value.value" :error-messages="email.errorMessage.value"
+                        label="E-mail"></v-text-field>
 
-                        <v-select v-model="select.value.value" :items="items" :error-messages="select.errorMessage.value"
-                            label="In which session are you going to participate?"></v-select>
+                    <v-select v-model="select.value.value" :items="items" :error-messages="select.errorMessage.value"
+                        label="In which session are you going to participate?"></v-select>
 
-                        <v-btn class="me-4" type="submit">
-                            Submit
-                        </v-btn>
+                    <v-btn class="me-4" type="submit">
+                        Submit
+                    </v-btn>
 
-                        <v-btn @click="handleReset">
-                            Clear
-                        </v-btn>
-                    </form>
+                    <v-btn @click="handleReset">
+                        Clear
+                    </v-btn>
+                </form>
 
-                </v-card>
+            </v-card>
         </v-container>
     </v-main>
 </template>
@@ -33,7 +33,7 @@
 import { ref } from 'vue'
 import { useField, useForm } from 'vee-validate'
 import axios from 'axios'
-import BaseCard from '@/layouts/default/BaseCard.vue';
+import Swal from 'sweetalert2'
 
 const { handleSubmit, handleReset } = useForm({
     validationSchema: {
@@ -65,27 +65,39 @@ const items = ref([
 ])
 
 const submit = handleSubmit(values => {
-    alert(JSON.stringify(values, null, 2));
-    axios.post("https://aiworkshop-69bd8-default-rtdb.firebaseio.com/submissions.json", values)
+    axios.post("https://aiworkshop-69bd8-default-rtdb.firebaseio.com/submissions.jsn", values)
         .then(function (response) {
-            console.log(response);
+            Swal.fire({
+                title: "Thank you for registering!",
+                text: "We are looking forward to see you.",
+                icon: "success"
+            });
         })
         .catch(function (error) {
             console.log(error);
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Something went wrong! Try to contact the event organization.",
+            });
         });
 })
 </script>
 
 <style scoped>
-
 .background-image {
-  background-image: url('/fu-university.jpeg'); /* Replace with your image path */
-  background-size: cover;
-  background-position: center;
-  min-height: 100vh; /* Full viewport height */
-  width: 100vw; /* Full viewport width */
-  padding: 0; /* Remove padding */
-  margin: 0; /* Remove margin */
+    background-image: url('/fu-university.jpeg');
+    /* Replace with your image path */
+    background-size: cover;
+    background-position: center;
+    min-height: 100vh;
+    /* Full viewport height */
+    width: 100vw;
+    /* Full viewport width */
+    padding: 0;
+    /* Remove padding */
+    margin: 0;
+    /* Remove margin */
 }
 
 .text-format {
