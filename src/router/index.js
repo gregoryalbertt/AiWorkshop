faf66289f866c15e15d38182cdc137bf145edc73
Kamilla Tenório <enkiski@gmail.com>
2023-12-05 12:57:20 +0100

    Revert "Updating the url for register"
    This reverts commit 0ba249b741a9c08e93cb45a9278f1e3a3715a3f4.

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const routes = [
         component: () => import("@/views/TheSchedule.vue"),
       },
       {
-        path: "register",
+        path: "subscribe",
         name: "Subscribe",
         component: () => import("@/views/TheForm.vue"),
       },
